e19770ec66049082fb35461b87431f8ca301443d
qarkly <qarkly@gmail.com>
2014-07-31 01:09:43 +0800

    leetCode 1 Two Sum

---
+++ LeetCode/src/org/qarkly/leetcode/twosum/Solution.java
@@ -0,0 +1,11 @@
+package org.qarkly.leetcode.twosum;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: qarkly
+ * Date: 14-7-31
+ * Time: 上午1:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class Solution {
+}
